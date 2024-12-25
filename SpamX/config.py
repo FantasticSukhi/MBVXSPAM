31df2bb7a272927f670fce3b261ba140aadf341d
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2024-12-26 00:51:01 +0530

    Update config.py

--- SpamX/config.py
+++ SpamX/config.py
@@ -6,32 +6,32 @@ from dotenv import load_dotenv
 if os.path.exists(".env"):
     load_dotenv(".env")
 
-API_ID = int(env("API_ID", None))
+API_ID = int(env("API_ID", 27251761))
 if API_ID is None:
     LOGS.error("Please set your API_ID!")
     quit(1)
 
-API_HASH = str(env("API_HASH", None))
+API_HASH = str(env("API_HASH", 22561f023ab0dccfe3b609845014ff0b))
 if API_HASH is None:
     LOGS.error("Please set your API_HASH!")
     quit(1)
 
-ASSISTANT_TOKEN = str(env("ASSISTANT_TOKEN", None))
+ASSISTANT_TOKEN = str(env("ASSISTANT_TOKEN", 6873452469:AAG4mhD3lDpOBickoDdRznjTLM0FnF3w9jg))
 if ASSISTANT_TOKEN is None:
     LOGS.error("Please set your ASSISTANT_TOKEN!")
     quit(1)
 
-OWNER_ID = int(env("OWNER_ID", None))
+OWNER_ID = int(env("OWNER_ID", 6713994904))
 if OWNER_ID is None:
     LOGS.error("Please set your OWNER_ID!")
     quit(1)
 
-DATABASE_URL = str(env("DATABASE_URL", None))
+DATABASE_URL = str(env("DATABASE_URL", mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority))
 if DATABASE_URL is None:
     LOGS.error("Please set your DATABASE_URL!")
     quit(1)
 
-LOGGER_ID = str(env("LOGGER_ID", None))
+LOGGER_ID = str(env("LOGGER_ID", -1002138809373))
 if LOGGER_ID is None:
     LOGS.error("Please set your LOGGER_ID! (make a private group add assistant in that group & promote as Admin!)")
     quit(1)
